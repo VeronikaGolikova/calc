69965e9b0a63b724d7ae2d745a8d67f23765f835
Veronika_Golikova <golikova.veronika.igorevna@mail.ru>
2018-07-07 17:43:58 +0400

    Add. Добавлены методы вывода в консоль для лучшего понимания работы программы.

--- src/main/java/RandomNumber.java
+++ src/main/java/RandomNumber.java
@@ -8,6 +8,7 @@ public class RandomNumber {
         int minPositiveElement = n;
         int intArray[] = new int[n];
         Random random = new Random();
+        System.out.println("Сгенерирован массив из случайных чисел:");
         for (int i = 0; i < intArray.length; i++) {
             intArray[i] = random.nextInt(2 * n + 1) - n;
             System.out.print(intArray[i] + " ");
@@ -27,6 +28,7 @@ public class RandomNumber {
                 intArray[i] = maxMinusElement;
             }
         }
+        System.out.println("\n Массив после перемещения элементов:");
         System.out.println();
         for (int i = 0; i < intArray.length; i++) {
             System.out.print(intArray[i] + " ");
